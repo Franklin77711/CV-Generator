31d2d5eae421314dabd80f96832beb0480609d5a
Franklin77711 <robi1000350@gmail.com>
2023-01-04 16:28:12 +0100

    Added education section, need debugging

--- cv-generator/src/App.js
+++ cv-generator/src/App.js
@@ -20,6 +20,14 @@ class App extends Component{
         id: uniqid(),
     },
       generals: [],
+
+      education: {
+        school: '',
+        subject: '',
+        time: '',
+        id: uniqid(),
+      },
+      educations:[]
     };
   }
   handleChangeName = (e) =>{
@@ -53,42 +61,88 @@ class App extends Component{
       }
     });
   };
+  handleChangeSchool = (e) =>{
+    this.setState({
+      education:{
+        school: e.target.value,
+        subject: this.state.education.subject,
+        time: this.state.education.time,
+        id: this.state.education.id,
+      }
+    });
+  };
+  handleChangeSubject = (e) =>{
+    this.setState({
+      education:{
+        school: this.state.education.school,
+        subject: e.target.value,
+        time: this.state.education.time,
+        id: this.state.education.id,
+      }
+    });
+  };
+  handleChangeTime = (e) =>{
+    this.setState({
+      education:{
+        school: this.state.education.school,
+        subject: this.state.education.subject,
+        time: e.target.value,
+        id: this.state.education.id,
+      }
+    });
+  };
 
   submitAction = (e) =>{
     e.preventDefault();
     this.setState({
       generals: this.state.generals = [],
       generals: this.state.generals.concat(this.state.general),
+      educations: this.state.educations.concat(this.state.education),
       general: {
         name: "",
         address:"",
         email: "",
         id: uniqid(),
       },
+      education: {
+        school: "",
+        subject:"",
+        time: "",
+        id: uniqid(),
+      },
     });
   };
   render(){
-    const {general, generals} = this.state;
+    const {general, generals, education, educations} = this.state;
     return(
       <div>
         <h1 id="main-title">CV Generator</h1>
         <div id="content">
           <div id="form-side">
           <form onSubmit={this.submitAction}>
-            <h3 htmlFor="General" className="subhead">General Information</h3>
+            <h3 className="subhead">General Information</h3>
             <label htmlFor="name">Name:</label>
             <input id="name" value={general.name} onChange={this.handleChangeName}></input>
             <label htmlFor="address">Address:</label>
             <input id="address" value={general.address} onChange={this.handleChangeAddress}></input>
             <label htmlFor="email">Email:</label>
             <input id="email" value={general.email} onChange={this.handleChangeEmail}></input>
+
+            <h3 className="subhead">Education</h3>
+            <label htmlFor="school">School Name:</label>
+            <input id="school" value={education.school} onChange={this.handleChangeSchool}></input>
+            <label htmlFor="subject">Subject:</label>
+            <input id="subject" value={education.subject} onChange={this.handleChangeSubject}></input>
+            <label htmlFor="time">From - To:</label>
+            <input id="time" value={education.time} onChange={this.handleChangeTime}></input>
+          
             <button type="submit" >Generate CV</button>
           </form>
           </div>
           <div id="generated-side">
             <div id="generated-content">
               <General generals={generals}/>
-              <Education/>
+              <Education educations={educations}/>
               <Experience/>
             </div>
           </div>
